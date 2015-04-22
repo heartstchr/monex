e809f546ba2981e6e93ff51637420a01813fdae6
heartstchr <heartstchr@live.com>
2015-04-22 10:38:16 +0530

    mongoDb as backend configured

--- mongoDb/wsgi.py
+++ mongoDb/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for monex project.
+WSGI config for mongoDb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mongoDb.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monex.settings")
-
 application = get_wsgi_application()
